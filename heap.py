29ca1059cd5b45752e1c2254283749c7763bd1cd
DuyPham <duy.cs.pham@gmail.com>
2022-11-27 23:41:14 -0600

    basic heap

--- heap.py
+++ heap.py
@@ -1,3 +1,19 @@
-H = [None] * 5
+class Heap:
+    def __init__(self, num):
+        self.H = [None] * num
+        self.D = [None] * num
+        self.P = [None] * num
+    
+    def findPosition(self, id):
+        return self.P[id]
 
-print(H[4])
+    def getValue(self, id):
+        return self.D[id]
+
+    def maximum(self):
+        return [self.H[0], self.D[self.H[0]]]
+
+    def insert(self, id, value):
+        self.D[id] = value
+        
+
